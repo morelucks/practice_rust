b609b69eaead6bb72b9befdd1f180658f6bb5bb6
morelucks <luckykamshak@gmail.com>
2024-06-13 18:08:58 +0000

    boolean

--- src/types.rs
+++ src/types.rs
@@ -1,11 +1,19 @@
 pub fn run(){
 
-    let x=1;
-    let y=2.5;
-    let y:i64 =4444444847;
+let is_active:bool=true;
+let off:bool=false;
+let is_less:bool=40<190;
 
-    println!("{}", std::i32::MAX);
-    println!("{}", std::f64::MAX);
+// let is_less:bool=40>190;
+
+println!("{:?}", (is_active, off, is_less))
+
+    // let x=1;
+    // let y=2.5;
+    // let y:i64 =4444444847;
+
+    // println!("{}", std::i32::MAX);
+    // println!("{}", std::f64::MAX);
     // let hell= String::from("hello");
     // println!("{}", hell);
 }
