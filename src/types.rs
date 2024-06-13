ffa99263cdaaab90175624304eace622279caf13
morelucks <luckykamshak@gmail.com>
2024-06-13 17:25:19 +0000

    practice

---
+++ src/types.rs
@@ -0,0 +1,11 @@
+pub fn run(){
+
+    let x=1;
+    let y=2.5;
+    let y:i64 =4444444847;
+
+    println!("{}", std::i32::MAX);
+    println!("{}", std::f64::MAX);
+    // let hell= String::from("hello");
+    // println!("{}", hell);
+}
